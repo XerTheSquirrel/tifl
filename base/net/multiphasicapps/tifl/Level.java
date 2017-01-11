aa47089895df8e736c473e457ca3d80e8d4598cb
Steven Gawroriski <steven@multiphasicapps.net>
2017-01-11 16:12:51 +0000

    Add some base classes.

---
+++ base/net/multiphasicapps/tifl/Level.java
@@ -0,0 +1,24 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: Trouble In Furry Land
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// Trouble In Furry Land is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.tifl;
+
+/**
+ * This contains the actual level data for the game and is used to represent
+ * the level.
+ *
+ * Entities are not contained within the level but in {@link Simulation}.
+ *
+ * @since 2017/01/11
+ */
+public class Level
+{
+}
+
---
+++ base/net/multiphasicapps/tifl/Simulation.java
@@ -0,0 +1,22 @@
+// -*- Mode: Java; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: Trouble In Furry Land
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// Trouble In Furry Land is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+package net.multiphasicapps.tifl;
+
+/**
+ * This contains the game simulation which contains the level, entities, and
+ * performs the basic game loop.
+ *
+ * @since 2017/01/11
+ */
+public class Simulation
+{
+}
+
