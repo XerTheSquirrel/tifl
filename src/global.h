df9fb4e0bab365c72f2cd00832922831c402a9f3
Steven Gawroriski <steven@multiphasicapps.net>
2017-01-12 22:03:20 +0000

    Bring in fixed point and angle types from an older one of my games.

--- src/global.h
+++ src/global.h
@@ -30,8 +30,39 @@ typedef enum
 	true
 } boolean;
 
-/** Angle type (BAM). */
+#define FIXEDSHIFT ((int32_t)16)
+#define FIXED_C(x) (INT32_C(x) << FIXEDSHIFT)
+#define FIXEDONE FIXED_C(1)
+#define FIXEDHALF ((int32_t)INT32_C(0x8000))
+#define FIXEDMASK ((int32_t)INT32_C(0xFFFF))
+
+#define fxtoi(x) (((int32_t)(x)) >> FIXEDSHIFT)
+#define itofx(x) (((int32_t)(x)) << FIXEDSHIFT)
+
+typedef int32_t fixedtype;
+
+fixedtype FixedMul(const fixedtype a_A, const fixedtype a_B);
+fixedtype FixedDiv(const fixedtype a_A, const fixedtype a_B);
+int32_t OctoDist(const int32_t a_X1, const int32_t a_Y1,
+	const int32_t a_X2, const int32_t a_Y2);
+
+#define ANGLETYPE_C(x) (UINT32_C(x))
+
+#define ANG0	ANGLETYPE_C(0x00000000)
+#define ANG1	ANGLETYPE_C(0x00b60b60)
+#define ANG45	ANGLETYPE_C(0x20000000)
+#define ANG90	ANGLETYPE_C(0x40000000)
+#define ANG135	ANGLETYPE_C(0x60000000)
+#define ANG180	ANGLETYPE_C(0x80000000)
+#define ANG270	ANGLETYPE_C(0xC0000000)
+
 typedef uint32_t angletype;
+
+fixedtype AngleSin(const angletype a_An);
+fixedtype AngleCos(const angletype a_An);
+
+angletype LineToAngle(const int32_t a_X1, const int32_t a_Y1,
+	const int32_t a_X2, const int32_t a_Y2);
 
 /**
  * Kills the program.
