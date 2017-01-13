d890bb6f9c3308ca04a04943d37aeaa1a929bdbc
Steven Gawroriski <steven@multiphasicapps.net>
2017-01-13 16:29:26 +0000

    Slow the player down a bit.

--- src/main.c
+++ src/main.c
@@ -22,7 +22,7 @@
 #define PLAYER_TURN_ANGLE 0x02d82d80
 
 /** The speed the player moves at. */
-#define PLAYER_MOVE_SPEED (1 << (FIXEDSHIFT - 1))
+#define PLAYER_MOVE_SPEED ((1 << (FIXEDSHIFT - 1)) - (1 << (FIXEDSHIFT - 3)))
 
 void Die(const char* format, ...)
 {
