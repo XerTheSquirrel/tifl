19d01ce0856422cbb8e7be9fb9d194e364ddd618
Steven Gawroriski <steven@multiphasicapps.net>
2017-01-16 03:26:46 +0000

    Add script to make a windows binary.

--- utils/makewin.sh
+++ utils/makewin.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/make
+#!/bin/sh
 # ---------------------------------------------------------------------------
 # Multi-Phasic Applications: Trouble In Furry Land
 #     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
@@ -7,28 +7,30 @@
 # Trouble In Furry Land is under the GNU General Public License v3+, or later.
 # See license.mkd for licensing and copyright information.
 # ---------------------------------------------------------------------------
-# DESCRIPTION: Makefile for Trouble In Furry Land
+# DESCRIPTION: Makes windows binary
 
-SDL_CONFIG=sdl2-config
-EXESUFFIX=
+# Force C locale
+export LC_ALL=C
 
-CFLAGS  := $(CFLAGS) $(shell $(SDL_CONFIG) --cflags) \
-	-Werror=implicit-function-declaration
-LDFLAGS := $(LDFLAGS) $(shell $(SDL_CONFIG) --libs) -lm
+# Directory of this script
+__exedir="$(dirname -- "$0")"
 
-.PHONY: all
-all:			tifl$(EXESUFFIX)
-				
+# Go to source location
+cd "$__exedir/.."
 
-__objects := $(foreach __c,$(wildcard src/*.c),$(basename $(__c)).o)
-__headers := $(wildcard src/*.h)
+# Clean up local
+make clean
+make clean EXESUFFIX=.exe
 
-tifl$(EXESUFFIX):			$(__objects)
-				$(CC) -o $@ $^ $(LDFLAGS)
+# Compile for Windows
+__sdldir="/usr/local/cross-tools/i686-w64-mingw32/"
+if make CC=i686-w64-mingw32-gcc SDL_CONFIG="$__sdldir/bin/sdl2-config" EXESUFFIX=.exe
+then
+	cp "$__sdldir/bin/"*.dll .
+	echo "yay"
+	
+	rm tifl-win32.zip
+	zip tifl-win32.zip *.dll tifl.exe readme.mkd license.mkd
+fi
 
-%.o:			src/%.c $(__headers)
-				$(CC) -o $@ -c $< $(CFLAGS)
-
-clean:			
-				rm -f -- $(__objects) tifl
 
