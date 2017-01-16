7ad795835be42adff64f66475ba12638208b15e2
Steven Gawroriski <steven@multiphasicapps.net>
2017-01-16 02:40:59 +0000

    Base for sound, although I might just skip it.

---
+++ src/sound.c
@@ -0,0 +1,21 @@
+// -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: Trouble In Furry Land
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// Trouble In Furry Land is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+#include <SDL.h>
+
+#include "sound.h"
+
+/** Sound is available. */
+static boolean soundok;
+
+void InitSound()
+{
+}
+
---
+++ src/sound.h
@@ -0,0 +1,22 @@
+// -*- Mode: C; indent-tabs-mode: t; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// Multi-Phasic Applications: Trouble In Furry Land
+//     Copyright (C) Steven Gawroriski <steven@multiphasicapps.net>
+//     Copyright (C) Multi-Phasic Applications <multiphasicapps.net>
+// ---------------------------------------------------------------------------
+// Trouble In Furry Land is under the GNU General Public License v3+, or later.
+// See license.mkd for licensing and copyright information.
+// ---------------------------------------------------------------------------
+
+#ifndef TIFL_SOUND_H
+#define TIFL_SOUND_H
+
+/**
+ * Initializes sound, or at least tries to.
+ *
+ * @since 2017/01/15
+ */
+void InitSound();
+
+#endif
+
